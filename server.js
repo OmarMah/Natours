1f8f018d016edf2386f49b794aa1046d1d1ac949
OmarMah <36341168+OmarMah@users.noreply.github.com>
2024-08-25 04:42:47 +0300

    Added the remote database connection

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const app = require('./app');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const port = 3000; 
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose.connect(DB).then(() => {
+    console.log('DB connection successful');
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
