3ddc3b060b22a1a369af6e11350e083312f92e45
OmarMah <36341168+OmarMah@users.noreply.github.com>
2024-08-26 03:39:56 +0300

    Configured the tours' controller to create new documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -22,13 +22,23 @@ exports.getTour = (req,res) => {
     // });
 };
 
-exports.createTour = (req, res) => {
-    res.status(201).json({
-        status: 'success',
-        data: {
-            tour: newTour
-        }
-    });
+exports.createTour = async (req, res) => {
+
+    try{
+        const newTour = await Tour.create(req.body);
+    
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        });
+    } catch (err) {
+        res.status(400).json({
+            status:'fail',
+            message: err,
+        });
+    }
 };
 
 exports.updateTour = (req, res) => {
@@ -45,15 +55,5 @@ exports.deleteTour = (req, res) => {
         status: 'success',
         data: null
     });
-}
-
-exports.checkBody = (req,res,next) => {
-    if(!req.body.name || !req.body.price){
-        return res.status(400).json({
-            status: 'fail',
-            message: 'Missing name or price'
-        });
-    }
-    next();
 }
     
